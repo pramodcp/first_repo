223f2904057f8e8da8759955702fcbec075e58b7
pramodcp <archanapatil194@gmail.com>
2023-02-26 14:22:26 +0530

    modified code

--- smallest_number.py
+++ smallest_number.py
@@ -1,11 +1,11 @@
 #smallest number
-a = float(input("enter first number:"))
-b = float(input("enter second number:"))
-c = float(input("enter third number:"))
+a = eval(input("enter first number:"))
+b = eval(input("enter second number:"))
+c = eval(input("enter third number:"))
 if (a<b) and (a<c):
     small=a
 elif (b<a) and (b<c):
     small=b
 else:
     small=c
-print("the smallest number is",small)
+print(f"The smallest number is {small} from given number {a},{b},{c}")
