45703abd911c173cf6a692d8486d2cde0c95f6ea
pramodcp <archanapatil194@gmail.com>
2023-02-27 22:31:16 +0530

    division_code

--- addition.py
+++ addition.py
@@ -3,3 +3,8 @@ a = int(input("enter first value:"))
 b = int(input("enter second value:"))
 add = a+b
 print(f"addition of {a} and {b} is {add}")
+
+a = int(input("enter first value:"))
+b = int(input("enter second value:"))
+div = a/b
+print(f"addition of {a} and {b} is {div}")
