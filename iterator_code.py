cd8f98a923746900c964da90477347e4f0d24803
pramodcp <archanapatil194@gmail.com>
2023-03-18 10:15:17 +0530

    iterator programm

---
+++ iterator_code.py
@@ -0,0 +1,10 @@
+iterable_value  = 'shreeraj'
+iterable_obj = iter(iterable_value)
+print(iterable_obj)
+print(next(iterable_obj))
+# # # print(next(iterable_obj))
+# # print(list(iterable_obj))
+#
+# for i in iterable_value :
+#     iterable_obj = iter(i)
+#     print(i)
